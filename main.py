73b6a9991ec80488d20a3c6e8305a390e9a8a2ac
Bumblebee99YT <71513684+Bumblebee99YT@users.noreply.github.com>
2022-07-21 12:56:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import nextcord
+from nextcord.ext import commands
+
+intents = nextcord.Intents().all()
+client = commands.Bot(command_prefix="ng.", intents=intents)
+
+@client.event
+async def on_ready():
+    print("Yo wsg bbg")
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("bro shut yo btch a$$ up")
+
+client.run("")
